cb6b28bf7e9643b85751620867312afd93e376d4
zahraa adnan <73762285+Zahraahaide@users.noreply.github.com>
2024-05-10 13:31:39 +0200

    updatED

--- src/tests/java/AsteroidMiningTests/RobotTest.java
+++ src/tests/java/AsteroidMiningTests/RobotTest.java
@@ -1,4 +1,4 @@
-package AsteroidMining;
+package AsteroidMiningTests;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
--- src/tests/java/AsteroidMiningTests/SettlerTest.java
+++ src/tests/java/AsteroidMiningTests/SettlerTest.java
@@ -1,4 +1,4 @@
-package AsteroidMining;
+package AsteroidMiningTests;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
